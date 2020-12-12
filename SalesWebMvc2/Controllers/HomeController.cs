86b5bc2057953109fbcfc320d9608374658eb6ec
Daniel Castro <d.13castrosilva@gmail.com>
2020-12-12 00:05:18 -0300

    First Model-Controller-View - Department

--- SalesWebMvc2/Controllers/HomeController.cs
+++ SalesWebMvc2/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc2.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Meu primeiro siteb com .Net.";
+            ViewData["aluno"] = "Daniel Castro";
 
             return View();
         }
